ab58d8929dd181e2a81840761442ce65a057a096
0xDV <di593297724ma@gmail.com>
2024-09-05 20:51:55 +0200

    added styles and model

--- components/Scene.tsx
+++ components/Scene.tsx
@@ -1,15 +1,22 @@
-'use client';
+'use client'
 
-import { Canvas, useThree } from '@react-three/fiber';
+import { Canvas, useThree } from '@react-three/fiber'
 import { Suspense } from 'react'
 import Model from './Model'
+import { useProgress, Html, ScrollControls } from '@react-three/drei'
+function Loader() {
+	const { progress, active } = useProgress()
 
+	return <Html center>{progress.toFixed(1)} % loaded</Html>
+}
 export default function Scene() {
-	return(
-		<Canvas>
-			<directionalLight position={[-5, -5, 5]} intensity={4}/>
-			<Suspense fallback={null}>
-				<Model/>
+	return (
+		<Canvas gl={{ antialias: true }} dpr={[1, 1.5]}   style={{ height: '100vh', width: '100vw' }}>
+			<directionalLight position={[-5, -5, 5]} intensity={4} />
+			<Suspense fallback={<Loader />}>
+				<ScrollControls damping={0.2} pages={2}>
+					<Model />
+				</ScrollControls>
 			</Suspense>
 		</Canvas>
 	)
