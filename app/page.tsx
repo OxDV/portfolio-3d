3be9011ceee4c09c1ece362870de7072ec8ea4fa
0xDV <di593297724ma@gmail.com>
2024-09-05 21:42:22 +0200

    added nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
 import dynamic from 'next/dynamic';
+import '../styles/styles.css'
 
 const Scene = dynamic(() => import('@/components/Scene'), { ssr: false });
 
 export default function Home() {
   return (
-    <main>
-      <Scene/>
-    </main>
+    <div>
+      <header>
+        <nav>
+          <ul id='navigation-buttons'>
+            <li>Logo</li>
+            <li>About Me</li>
+            <li>Skills</li>
+            <li>Write to Email</li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Scene/>
+      </main>
+    </div>
   );
 }
